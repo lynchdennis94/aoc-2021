3fb8f921daaaa3924f8c3e29323cbca005c4fc1e
Dennis Lynch <lynch.dennis94@gmail.com>
2021-12-12 11:41:36 -0800

    Solution for day 3 part one - bug in day 3 part two, the ones/zeros arrays aren't being recalculated post-filter

--- aoc-2021/src/main.rs
+++ aoc-2021/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::io::Read;
+use std::str::Split;
 
 fn main() {
     // Accept args to get solutions
@@ -17,6 +18,8 @@ fn main() {
             "1B" => solution_1b(),
             "2A" => solution_2a(),
             "2B" => solution_2b(),
+            "3A" => solution_3a(),
+            "3B" => solution_3b(),
             _ => println!("NOT SUPPORTED")
         };
     }
@@ -129,6 +132,118 @@ fn solution_2b() {
     }
 
     println!("Combined positions: {}", horizontal_position*vertical_position);
+}
+
+// Day 3 - part 1
+fn solution_3a() {
+    println!("Solving 3A");
+    //let file_location = "problem_3_sample.txt";
+    let file_location = "problem_3_input.txt";
+    let problem_input_string = get_problem_input(file_location);
+
+    let problem_input_split = problem_input_string.trim().split('\n');
+    let problem_input_vec = problem_input_split.collect::<Vec<&str>>();
+    let array_length = problem_input_vec[0].len();
+    let (ones_array, zeros_array) = get_most_common_bit_for_positions(problem_input_vec, array_length);
+
+    let mut answer_index = 0;
+    let mut binary_gamma_rate = "".to_owned(); // most common bit
+    let mut binary_epsilon_rate = "".to_owned(); // least common bit
+
+    while answer_index < array_length {
+        if ones_array[answer_index] >= zeros_array[answer_index] {
+            binary_gamma_rate.push_str("1");
+            binary_epsilon_rate.push_str("0");
+        } else {
+            binary_gamma_rate.push_str("0");
+            binary_epsilon_rate.push_str("1");
+        }
+        answer_index += 1;
+    }
+
+    let gamma_rate = isize::from_str_radix(binary_gamma_rate.as_str(), 2).unwrap();
+    let epsilon_rate = isize::from_str_radix(binary_epsilon_rate.as_str(), 2).unwrap();
+
+    println!("Power level: {}", gamma_rate*epsilon_rate);
+}
+
+// Day 3 - part 2
+fn solution_3b() {
+    println!("Solving 3B");
+    let file_location = "problem_3_sample.txt";
+    //let file_location = "problem_3_input.txt";
+    let problem_input_string = get_problem_input(file_location);
+
+    let problem_input_split = problem_input_string.trim().split('\n');
+    let problem_input_vec = problem_input_split.collect::<Vec<&str>>();
+    let mut oxygen_problem_set = problem_input_vec.clone();
+    let mut carbon_problem_set = problem_input_vec.clone();
+    let array_length = problem_input_vec[0].len();
+    let (ones_array, zeros_array) = get_most_common_bit_for_positions(problem_input_vec, array_length);
+
+    println!("Ones array: {:?}", ones_array);
+    println!("Zeroes array: {:?}", zeros_array);
+
+    // Oxygen - filter out numbers that don't adhere to most common bits
+    for filter_index in 0..array_length {
+        let mut working_problem_set: Vec<&str> = Vec::new();
+        let most_common_bit = if ones_array[filter_index] >= zeros_array[filter_index] {'1'} else {'0'};
+        //println!("At filter index {}, the mcb is {}", filter_index, most_common_bit);
+        for entry in oxygen_problem_set {
+            if entry.chars().nth(filter_index).unwrap() == most_common_bit {
+                // We've passed this filter, so continue
+                working_problem_set.push(entry);
+            }
+        }
+        oxygen_problem_set = working_problem_set.clone();
+        if oxygen_problem_set.len() == 1 {
+            break;
+        }
+    }
+
+    // Carbon - filter out numbers that don't adhere to least common bits
+    for filter_index in 0..array_length {
+        let mut working_problem_set: Vec<&str> = Vec::new();
+        let least_common_bit = if ones_array[filter_index] >= zeros_array[filter_index] {'0'} else {'1'};
+        //println!("At filter index {}, the mcb is {}", filter_index, most_common_bit);
+        for entry in carbon_problem_set {
+            if entry.chars().nth(filter_index).unwrap() == least_common_bit {
+                // We've passed this filter, so continue
+                working_problem_set.push(entry);
+            }
+        }
+        carbon_problem_set = working_problem_set.clone();
+        if carbon_problem_set.len() == 1 {
+            break;
+        }
+    }
+
+    let oxygen_rate = isize::from_str_radix(oxygen_problem_set.get(0).unwrap(), 2).unwrap();
+    let carbon_rate = isize::from_str_radix(carbon_problem_set.get(0).unwrap(), 2).unwrap();
+
+    println!("Live support rating: {}", oxygen_rate*carbon_rate);
+}
+
+// Helper function for day 3 = determine most common bit in each position
+fn get_most_common_bit_for_positions(problem_input_vec: Vec<&str>, array_length: usize) -> (Vec<i32>, Vec<i32>) {
+
+    let mut ones_array = vec![0; array_length];
+    let mut zeros_array = vec![0; array_length];
+
+    // Calculate the total number of ones/zeros in each bit position for everything in input
+    for entry in problem_input_vec {
+        let mut entry_index = 0;
+        while entry_index < array_length {
+            if entry.chars().nth(entry_index).unwrap() == '1' {
+                ones_array[entry_index] += 1;
+            } else {
+                zeros_array[entry_index] += 1;
+            }
+            entry_index += 1;
+        }
+    }
+
+    return (ones_array, zeros_array);
 }
 
 // Helper function to get the problem input
